e55ccf59eaf8e42bd503c5ac757c37ddad0ff066
Zachary Klein <zak@silver-chalice.com>
2016-11-04 14:21:47 -0700

    added client and server render functions

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
 import About from './about';
 
-require('./../styles/style.css');
 
-ReactDOM.render(<About />, document.getElementById('app'));
+if (typeof window !== 'undefined' && typeof document !== 'undefined' && typeof document.createElement === 'function') {
+  window.renderClient = (data) => {
+    ReactDOM.render(<About source={data.source} />, document.getElementById('app'));
+  }
+}
+else {
+  global.renderServer = (data) => {
+    let json = JSON.parse(data);
+    return ReactDOMServer.renderToString (
+      <About source={json.source} />
+    )
+  };
+}
